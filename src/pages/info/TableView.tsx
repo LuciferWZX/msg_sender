35dca8fac14e57b3f7aa3ab684233d5143e5a333
LuciferWZX <2396423791@qq.com>
2022-01-26 00:59:20 +0800

    数据解析

--- src/pages/info/TableView.tsx
+++ src/pages/info/TableView.tsx
@@ -1,23 +1,34 @@
-import { Button, Input, Space, Tag } from 'antd';
-import React, { ChangeEvent, FC, useEffect, useState } from 'react';
-import { ActionBox, SearchBox, TableBox, TableViewBox } from './style';
+import { Button, Input, Space, Tag, Tooltip } from 'antd';
+import React, { ChangeEvent, FC, memo, useEffect, useState } from 'react';
+import {
+  ActionBox,
+  BaseTableWithDarkTheme,
+  SearchBox,
+  TableBox,
+  TableViewBox,
+} from './style';
 import { useSelector } from 'umi';
-import Highlighter from 'react-highlight-words'
+import cx from 'classnames';
+import Highlighter from 'react-highlight-words';
 import { useDebounceFn, useMemoizedFn, useSize, useUpdateEffect } from 'ahooks';
-import { ArtColumn, BaseTable, features, useTablePipeline } from 'ali-react-table';
-import { SearchOutlined } from '@ant-design/icons';
-import PinyinMatch from 'pinyin-match'
+import { ArtColumn, features, useTablePipeline } from 'ali-react-table';
+import {
+  MinusCircleOutlined,
+  SearchOutlined,
+  SendOutlined,
+} from '@ant-design/icons';
+import PinyinMatch from 'pinyin-match';
 const TableView: FC = () => {
   const db = useSelector((state) => state.database.db);
   const excelData = useSelector((state) => state.database.excelData);
-  const [columns,setColumns]=useState<ArtColumn[]>([])
-  const [dataSource,setDataSource]=useState<any[]>([])
-  const [keyword,setKeyword]=useState<string>('')
-  const size = useSize(document.getElementById('table-view'))
+  const [columns, setColumns] = useState<ArtColumn[]>([]);
+  const [dataSource, setDataSource] = useState<any[]>([]);
+  const [keyword, setKeyword] = useState<string>('');
+  const size = useSize(document.getElementById('table-view'));
 
   const { run } = useDebounceFn(
-    (e:ChangeEvent<HTMLInputElement>)=>{
-      setKeyword(e.target.value)
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setKeyword(e.target.value);
     },
     {
       wait: 500,
@@ -25,132 +36,160 @@ const TableView: FC = () => {
   );
   //表格的配置
   const pipeline = useTablePipeline()
-    .input({dataSource:dataSource,columns})
-    .use(features.columnResize({
+    .input({ dataSource: dataSource, columns })
+    .use(
+      features.columnResize({
         //fallbackSize: 120,
         //handleBackground: '#ddd',
         handleHoverBackground: 'rgba(0, 168, 255,0.2)',
         handleActiveBackground: '#89bff7',
-      }))
+      }),
+    )
     .use(features.columnRangeHover())
-    .use(features.sort({
+    .use(
+      features.sort({
         mode: 'multiple',
         highlightColumnWhenActive: true,
-        clickArea:'icon'
-      }))
-  const renderSearchWords=(text:string,result:[number,number]):string=>{
-    return text.substring(result[0], result[1] + 1)
-  }
-  useEffect(()=>{
-    fetchColumnsData()
-    fetchDataSource()
-  },[excelData])
-  useUpdateEffect(()=>{
-    fetchDataSource()
-  },[keyword])
+        clickArea: 'icon',
+      }),
+    );
+  const renderSearchWords = (
+    text: string,
+    result: [number, number],
+  ): string => {
+    return text.substring(result[0], result[1] + 1);
+  };
+  useEffect(() => {
+    fetchColumnsData();
+    fetchDataSource();
+  }, [excelData]);
+  useUpdateEffect(() => {
+    fetchDataSource();
+  }, [keyword]);
   //渲染列
-  const fetchColumnsData=useMemoizedFn(()=>{
-    if(excelData.length>0){
-      const titles = Object.keys(excelData[0]).concat("actions");
+  const fetchColumnsData = useMemoizedFn(() => {
+    if (excelData.length > 0) {
+      const titles = Object.keys(excelData[0]).concat('actions');
       setColumns(
-        titles.map((item,index)=>{
-          if(item === "actions"){
+        titles.map((item, index) => {
+          if (item === 'actions') {
             return {
-              code:item,
-              name:"操作",
-              lock:true,
-              getValue(_){
+              code: item,
+              name: '操作',
+              lock: true,
+              getValue(_) {
                 return (
                   <ActionBox>
-                    <Button type="link" size={'small'}>
-                      发送短信
-                    </Button>
-                    <Tag>xxxxxxxxxxxxxxxxxxxxxxx</Tag>
+                    <Space>
+                      <Tooltip title={'发送短信'}>
+                        <SendOutlined className={'action-icon'} />
+                      </Tooltip>
+                      <Tag icon={<MinusCircleOutlined />} color="default">
+                        未发送
+                      </Tag>
+                    </Space>
                   </ActionBox>
-                )
-              }
+                );
+              },
+            };
+          }
+          return {
+            code: item,
+            name: item,
+            features: { sortable: true },
+          };
+        }),
+      );
+    }
+  });
+  //渲染数据
+  const fetchDataSource = useMemoizedFn(() => {
+    setDataSource(
+      excelData.filter((item) => {
+        if (keyword === '') {
+          return true;
+        }
+        let isPass = false;
+        const values: any[] = Object.values(item);
+        for (let i = 0; i < values.length; i++) {
+          if (values[i]) {
+            const result: boolean | [number, number] = PinyinMatch.match(
+              values[i].toString().toUpperCase(),
+              keyword,
+            );
+            if (result) {
+              isPass = true;
             }
           }
-        return {
-          code:item,
-          name:item,
-          features: { sortable: true }
         }
-      })
-      )
-    }
-  })
-  //渲染数据
-  const fetchDataSource=useMemoizedFn(()=>{
-    setDataSource(excelData.filter(item=>{
-      if(keyword === ""){
-        return true
-      }
-      let isPass = false
-      const values:any[] = Object.values(item);
-      for (let i=0;i<values.length;i++){
-        if(values[i]){
-          const result:boolean|[number,number] = PinyinMatch.match(
-            values[i].toString().toUpperCase(),
-            keyword
-          )
-          if(result){
-            isPass = true
-          }
-
-        }
-      }
-      return isPass
-    }))
-  })
+        return isPass;
+      }),
+    );
+  });
   //设置过滤的关键字
-  const changeWord=(e:ChangeEvent<HTMLInputElement>)=>{
-    run(e)
-  }
-  console.log(keyword);
+  const changeWord = (e: ChangeEvent<HTMLInputElement>) => {
+    run(e);
+  };
   return (
-    <TableViewBox >
+    <TableViewBox>
       <SearchBox>
         <Space>
-          <Input allowClear={true} onChange={changeWord} size={'small'} placeholder={"请输入"} suffix={<SearchOutlined/>}/>
+          <Input
+            allowClear={true}
+            onChange={changeWord}
+            size={'small'}
+            placeholder={'请输入'}
+            suffix={<SearchOutlined />}
+          />
         </Space>
       </SearchBox>
-      <TableBox  id={'table-view'}>
-        <BaseTable
+      <TableBox id={'table-view'}>
+        <BaseTableWithDarkTheme
           {...pipeline.getProps()}
-          style={{  height: size?.height||300, overflow: 'auto' }}
+          style={{
+            height: size?.height || 300,
+            overflow: 'auto',
+          }}
+          useVirtual={true}
+          className={cx({ dark: true })}
           components={{
-            Cell:({ row, rowIndex, column, colIndex, tdProps }:any)=> {
-            // row 单元格的行数据
-            // rowIndex 单元格的行下标
-            // column 单元格的列配置
-            // colIndex 单元格的列下标
-            // tdProps <td> 元素的 props
-              const item = (tdProps as any).children
+            Cell: ({ row, rowIndex, column, colIndex, tdProps }: any) => {
+              // row 单元格的行数据
+              // rowIndex 单元格的行下标
+              // column 单元格的列配置
+              // colIndex 单元格的列下标
+              // tdProps <td> 元素的 props
+              const item = (tdProps as any).children;
               const result: any | [number, number] = PinyinMatch.match(
                 item.toString(),
-                keyword
-              )
-              if(result === false){
-                return <td {...tdProps} />
+                keyword,
+              );
+              if (result === false) {
+                return <td {...tdProps} />;
               }
-              return <td {...tdProps} children={<Highlighter
-                highlightClassName="highlight-class"
-                searchWords={[renderSearchWords(item.toString(),result)]}
-                autoEscape={true}
-                textToHighlight={item.toString()}
-              />} />
-            }
+              return (
+                <td
+                  {...tdProps}
+                  children={
+                    <Highlighter
+                      highlightClassName="highlight-class"
+                      searchWords={[renderSearchWords(item.toString(), result)]}
+                      autoEscape={true}
+                      textToHighlight={item.toString()}
+                    />
+                  }
+                />
+              );
+            },
           }}
-          getRowProps={()=>{
+          getRowProps={() => {
             return {
-              className:'row-class'
-            }
+              className: 'row-class',
+            };
           }}
         />
       </TableBox>
-
     </TableViewBox>
   );
 };
-export default TableView;
+export default memo(TableView);
--- src/pages/info/style.ts
+++ src/pages/info/style.ts
@@ -1,3 +1,4 @@
+import { BaseTable } from 'ali-react-table';
 import styled from 'styled-components';
 
 export const InfoViewBox = styled.div`
@@ -14,17 +15,36 @@ export const TableViewBox = styled.div`
   display: flex;
   flex-direction: column;
   overflow: hidden;
-  .row-class{
+  .row-class {
     word-break: break-word;
   }
 `;
-export const SearchBox = styled.div`
-`
+export const BaseTableWithDarkTheme = styled(BaseTable)`
+  &.dark {
+    --bgcolor: #333;
+    --header-bgcolor: #45494f;
+    --hover-bgcolor: #46484a;
+    --header-hover-bgcolor: #606164;
+    --highlight-bgcolor: #191a1b;
+    --header-highlight-bgcolor: #191a1b;
+    --color: #dadde1;
+    --header-color: #dadde1;
+    --lock-shadow: rgb(37 37 37 / 0.5) 0 0 6px 2px;
+    --border-color: #3c4045;
+  }
+`;
+export const SearchBox = styled.div``;
 export const TableBox = styled.div`
   flex: 1;
   overflow: hidden;
   height: 100%;
   margin-top: 10px;
-`
+`;
 export const ActionBox = styled.div`
-`
+  .action-icon {
+    :hover {
+      color: #0984e3;
+      cursor: pointer;
+    }
+  }
+`;
